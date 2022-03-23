19b058511b7794c76a9bbba1ac2410eac54bf53b
nandoi0 <97204056+nandoi0@users.noreply.github.com>
2022-03-23 13:40:35 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import json
+from flask import Flask, abort
+from mock_data import catalog
 
 app = Flask("Server")
 
@@ -10,5 +12,46 @@ def home():
 @app.route("/me")
 def about_me():
     return "Fernando Iribe"
+
+
+
+
+######################################################
+################# API ENDPOINTs ######################
+######################################################
+
+@app.route("/api/catalog", methods=["get"])
+def get_catalog():
+    return json.dumps(catalog)
+
+
+@app.route("/api/catalog", methods=["post"])
+def save_product():
+    pass
+
+# Get /api/catalog/count -> how many products exist in the catalog
+@app.route("/api/catalog/count")
+def product_count():
+    count = len(catalog)
+    return json.dumps(count)
+
+
+#Get/api/catalog/total => the sum of all products prices
+@app.route("/api/catalog/total")
+def total_of_catalog():
+    total = 0
+    for prod in catalog:
+        total += prod["price"]
+        
+    return json.dumps(total)
+
+@app.route("/api/product/<id>")
+def get_by_id(id):
+    for prod in catalog:
+        if prod["_id"] == id:
+           return json.dumps(prod)
+
+    
+    return abort(404, "No product with such ID")
 
 app.run(debug=True)
